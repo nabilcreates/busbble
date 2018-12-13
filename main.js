2c5c2cd27e10a094900d925909f9da1cac74cafb
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 11:06:01 +0800

    Changes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ ajax({
 }, function (data) {
 
     let jsondata = JSON.parse(data)
-    simply.subtitle(jsondata.services[0]);
+    simply.subtitle(jsondata.services[0].no);
 
     // for (var i = 0; i < jsondata.services.length; i++) {
     // }
