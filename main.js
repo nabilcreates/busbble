19c367283c55946429a3c3d101d766d70e816b64
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 11:31:55 +0800

    Testing...

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
 let busstopcode = 27301
+let jsondata;
+let clicked = 0;
 
-function rerunUI() {
-    simply.title(busstopcode);
+simply.title(busstopcode);
+ajax({
+    url: `https://arrivelah.herokuapp.com/?id=${busstopcode}`
+}, function (data) {
+
+    jsondata = JSON.parse(data)
+
+    simply.scrollable(true)
+    simply.style('mono')
+
+    clicked = 0;
+
+    // ADD TO STRING FOR EVERYTHING!
+    simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
+    `);
+
+    simply.subtitle(clicked)
+
+    // simply.body(``);
+    // simply.body(``,false);
+
+
+    // for (var i = 0; i < jsondata.services.length; i++) {
+    // }
+
+});
+
+setInterval(()=>{
     ajax({
         url: `https://arrivelah.herokuapp.com/?id=${busstopcode}`
     }, function (data) {
-
-        let jsondata = JSON.parse(data)
-
-
-        simply.scrollable(true)
-        simply.style('mono')
-
-        // ADD TO STRING FOR EVERYTHING!
-        simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
-
-    `);
-
-        // simply.body(``);
-        // simply.body(``,false);
-
-
-        // for (var i = 0; i < jsondata.services.length; i++) {
-        // }
-
+    
+        jsondata = JSON.parse(data)
+        clicked ++
     });
-}
-
+    
+},500)
