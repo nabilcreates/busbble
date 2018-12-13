5564df652d50d04f84d3d362e9b0e95dd185d85c
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 11:05:15 +0800

    Changes

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-
 simply.title('Hello World!');
-ajax({ url: 'https://arrivelah.herokuapp.com/?id=27301' }, function(data){
-  simply.subtitle(JSON.parse(data));
+ajax({
+    url: 'https://arrivelah.herokuapp.com/?id=27301'
+}, function (data) {
+
+    let jsondata = JSON.parse(data)
+
+    for (var i = 0; i < jsondata.services.length; i++) {
+        simply.subtitle(jsondata.services[0]);
+    }
+
 });
