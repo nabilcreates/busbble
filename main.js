23d58a018eeb8cf2b19d6bf8a6b7b107a64f44dd
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 11:39:40 +0800

    Testing...

--- main.js
+++ main.js
@@ -1,34 +1,12 @@
-let busstopcode = 27301
-let jsondata;
-let count = 0;
+let count = 0
 
-simply.title(busstopcode);
-ajax({
-    url: `https://arrivelah.herokuapp.com/?id=${busstopcode}`
-}, function (data) {
-
-    jsondata = JSON.parse(data)
-
-    simply.scrollable(true)
-
-    // ADD TO STRING FOR EVERYTHING!
-    simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
-    `);
-
-    simply.subtitle(count)
-
-    // simply.body(``);
-    // simply.body(``,false);
-
-
-    // for (var i = 0; i < jsondata.services.length; i++) {
-    // }
-
-    simply.text({ title: 'Counter', subtitle: count },false);
-    simply.text({ title: 'Hey', subtitle: 'count' },false);
-
-});
-
-simply.on('singleClick', function (e) {
-    count++
-});
+simply.on('singleClick', function(e) {
+    if (e.button === 'up') {
+      simply.subtitle(++count);
+    } else if (e.button === 'down') {
+      simply.subtitle(--count);
+    }
+    localStorage.setItem('count', count);
+  });
+  
+  simply.text({ title: 'Counter', subtitle: count });
