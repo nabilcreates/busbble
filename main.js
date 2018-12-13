e08d97c5bba0c4c61ac945e7c7df3812de3ca3f6
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 11:29:42 +0800

    Testing...

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 let busstopcode = 27301
 
-simply.title(busstopcode);
-ajax({
-    url: `https://arrivelah.herokuapp.com/?id=${busstopcode}`
-}, function (data) {
+function rerunUI() {
+    simply.title(busstopcode);
+    ajax({
+        url: `https://arrivelah.herokuapp.com/?id=${busstopcode}`
+    }, function (data) {
 
-    let jsondata = JSON.parse(data)
+        let jsondata = JSON.parse(data)
 
 
-    simply.scrollable(true)
-    simply.style('mono')
-    
-    // ADD TO STRING FOR EVERYTHING!
-    simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
-    ${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
-    ${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
+        simply.scrollable(true)
+        simply.style('mono')
+
+        // ADD TO STRING FOR EVERYTHING!
+        simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n ${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins \n
 
     `);
-    
-    // simply.body(``);
-    // simply.body(``,false);
+
+        // simply.body(``);
+        // simply.body(``,false);
 
 
-    // for (var i = 0; i < jsondata.services.length; i++) {
-    // }
+        // for (var i = 0; i < jsondata.services.length; i++) {
+        // }
 
-});
+    });
+}
+
