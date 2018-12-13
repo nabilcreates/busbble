665d12fef0f137806e3eae547acf08712ff8b221
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 11:18:20 +0800

    Testing...

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+let busstopcode = 27301
+
 simply.title('Hello World!');
 ajax({
-    url: 'https://arrivelah.herokuapp.com/?id=27301'
+    url: `https://arrivelah.herokuapp.com/?id=${busstopcode}`
 }, function (data) {
 
     let jsondata = JSON.parse(data)
 
-    // ADD TO STRING FOR EVERYTHING!
     
-    simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins`);
-    simply.body(`${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins`);
-    simply.body(`${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins`);
+    // ADD TO STRING FOR EVERYTHING!
+    simply.body(`${jsondata.services[0].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins`,false);
+    simply.body(`${jsondata.services[1].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins`,false);
+    simply.body(`${jsondata.services[2].no.toString()} is arriving in ${Math.floor(jsondata.services[0].next.duration_ms/60000)} Mins`,false);
 
 
     // for (var i = 0; i < jsondata.services.length; i++) {
