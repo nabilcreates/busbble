d7a06aee50abbb3440a15ec61347c3b7ce1b8a3c
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 10:58:12 +0800

    Changes

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-let data = 'COOL';
-
-fetch("https://arrivelah.herokuapp.com/?id=27301")
-.then(res => res.json())
-.then(json => {
-    data = json;
-})
 
 simply.title('Hello World!');
-simply.subtitle(data);
+ajax({ url: 'https://arrivelah.herokuapp.com/?id=27301' }, function(data){
+  simply.subtitle(data);
+});
