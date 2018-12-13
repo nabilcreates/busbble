9237662db56b3f65ceece62555073e7b4b6f8f51
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-13 10:57:06 +0800

    Changes

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+let data = 'COOL';
+
+fetch("https://arrivelah.herokuapp.com/?id=27301")
+.then(res => res.json())
+.then(json => {
+    data = json;
+})
+
 simply.title('Hello World!');
+simply.subtitle(data);
