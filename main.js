dc3db8e106d20157cedac259c281fb8c2ba1a8c8
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-14 09:25:10 +0800

    Changes - Serious Development

--- main.js
+++ main.js
@@ -3,14 +3,3 @@ simply.fullscreen(true)
 
 // SELECT BUS STOP NUMBER!!!
 simply.body('hey')
-
-// simply.on('singleClick', function (e) {
-//     if (e.button === 'up') {
-//         simply.subtitle(++count);
-//     } else if (e.button === 'down') {
-//         simply.subtitle(--count);
-//     } else if (e.button === 'select') {
-//         simply.subtitle(--count);
-//     }
-//     localStorage.setItem('count', count);
-// });
