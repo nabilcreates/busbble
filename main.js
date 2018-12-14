73bea593f96a50e7e88cf32f101ebe0a76f04ee8
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-14 11:22:21 +0800

    Testing for v0.0.2

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function check() {
 
 // DISPLAY THE SLECT BUS STOP CODE UI
 function displayUI() {
-    simply.title('Busbble v0.0.2r4')
+    simply.title('Busbble v0.0.2r5')
     simply.subtitle(bsn.join(" | "))
 
     // SHOWS THE CURRENT COUNT OVER THE MAX
