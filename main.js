87946a8f7ff4851dee8f02781c5927fb1ef8a6c8
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-14 09:35:24 +0800

    Changes - Serious Development

--- main.js
+++ main.js
@@ -8,8 +8,20 @@ simply.fullscreen(true)
 // SELECT BUS STOP NUMBER!!!
 
 // displays the bus stop number for now
-simply.body(bsn + '' + count)
+simply.body(bsn + '\n Count:' + count)
 
 simply.on('singleClick', function (e) {
-    simply.subtitle('You pressed the ' + e.button + ' button!');
+
+    // e.button
+    // select
+    // up
+    // down
+    
+    if (e.button === 'up') {
+        bsn[count]++
+    } else if (e.button === 'down') {
+        bsn[count]--
+    } else if (e.button === 'select') {
+        count++
+    }
 });
