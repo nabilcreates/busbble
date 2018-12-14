92a09cf91925a88352b8ffb3cc251fa483f815a6
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-14 09:31:02 +0800

    Changes - Serious Development

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ var b3 = 3;
 var b4 = 0;
 var b5 = 1;
 
+var myarray = [2,7,3,0,1]
+
 // CONFIG
 simply.fullscreen(true)
 
 // SELECT BUS STOP NUMBER!!!
-simply.body(b1 + b2 + b3 + b4 + b5)
+// simply.body(b1 + b2 + b3 + b4 + b5)
+simply.body(myarray)
