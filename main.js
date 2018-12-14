53fdde2399939120270d8d4a7f65d8832d00f3c8
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-14 09:32:58 +0800

    Changes - Serious Development

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var b1 = 2;
-var b1 = 2;
-var b2 = 7;
-var b3 = 3;
-var b4 = 0;
-var b5 = 1;
-
-var myarray = [2,7,3,0,1]
+var bsn = [2, 7, 3, 0, 1]
+var count = 0;
 
 // CONFIG
 simply.fullscreen(true)
 
 // SELECT BUS STOP NUMBER!!!
-// simply.body(b1 + b2 + b3 + b4 + b5)
-simply.body(myarray)
+
+// displays the bus stop number for now
+simply.body(bsn + '' + count)
+
+simply.on('singleClick', function (e) {
+    simply.subtitle('You pressed the ' + e.button + ' button!');
+});
