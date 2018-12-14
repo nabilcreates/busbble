75bb3c3edbfe49f5b71307bf17f9f65e73b3f986
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-12-14 09:27:16 +0800

    Changes - Serious Development

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let myarray = [42,33]
+let b1 = 2;
+let b1 = 7;
+let b1 = 3;
+let b1 = 0;
+let b1 = 1;
 
 // CONFIG
 simply.fullscreen(true)
 
 // SELECT BUS STOP NUMBER!!!
-simply.body(myarray)
+simply.body(`${b1}${b2}${b3}${b4}${b5}`)
