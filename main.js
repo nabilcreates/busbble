659d863eaed3848b4c67b6f12b44b4708b057995
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-01-16 20:47:21 +0800

    Replaced error

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function displayBusUI() {
     var minus = data.services.length - 1
     simply.title(bsn.join(""))
     simply.subtitle(currentservice.toString() + '/' + minus.toString())
-    simply.body(data.services[currentservice].no + toMins(data.services[currentservice].next.duration_ms) + data.services[currentservice].next.type);
+    simply.body(data.services[currentservice].no + toMins(data.services[currentservice].next.duration_ms) +  " [" + data.services[currentservice].next.type + "]");
 }
 
 function checkBus() {
